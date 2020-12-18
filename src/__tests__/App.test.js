ac8cfc2c8568cd6824f41f9e3b5c86a893f1723d
lekegitrepo <lekedevrepo@gmail.com>
2020-12-18 12:56:30 -0600

    add tests for the App component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../containers/App';
+
+//jest.mock('../containers/App')
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  const text = wrapper.find('p').text();
+  expect(text).toContain('Edit');
 });
