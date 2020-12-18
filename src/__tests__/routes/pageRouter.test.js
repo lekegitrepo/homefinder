cf8b325562fbd7b57ebd07db024493dd83708654
lekegitrepo <lekedevrepo@gmail.com>
2020-12-18 13:09:43 -0600

    add route test for the homedetail page

--- src/__tests__/routes/pageRouter.test.js
+++ src/__tests__/routes/pageRouter.test.js
@@ -5,6 +5,7 @@ import { MemoryRouter } from 'react-router';
 import Home from '../../components/pages/Home';
 import PageNotFound from '../../components/pages/PageNotFound.page';
 import Homes from '../../components/pages/Homes.index.page';
+import HomeDetail from '../../components/pages/Home.show.page';
 import PageRouter from '../../containers/pageRouter.container';
 
 test('Valid route path for the home page', () => {
@@ -35,4 +36,14 @@ test('Valid route for the homes list index page', () => {
   )
   expect(wrapper.find(PageNotFound)).toHaveLength(0)
   expect(wrapper.find(Homes)).toHaveLength(1)
+})
+
+test('Valid route for the home show page', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={['/home/:id']}>
+      <HomeDetail />
+    </MemoryRouter>
+  )
+  expect(wrapper.find(PageNotFound)).toHaveLength(0)
+  expect(wrapper.find(HomeDetail)).toHaveLength(1)
 })
