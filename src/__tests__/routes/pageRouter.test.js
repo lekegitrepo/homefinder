ac5cea786f76dfaf0ed5de4d50681b5279219dd2
lekegitrepo <lekedevrepo@gmail.com>
2020-12-18 12:57:26 -0600

    create routes test for home, pagenotfound, and homes pages

---
+++ src/__tests__/routes/pageRouter.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ReactDom from 'react-dom';
+import { MemoryRouter } from 'react-router';
+import Home from '../../components/pages/Home';
+import PageNotFound from '../../components/pages/PageNotFound.page';
+import Homes from '../../components/pages/Homes.index.page';
+import PageRouter from '../../containers/pageRouter.container';
+
+test('Valid route path for the home page', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={['/']}>
+      <Home />
+    </MemoryRouter>
+  )
+  expect(wrapper.find(PageNotFound)).toHaveLength(0)
+  expect(wrapper.find(Home)).toHaveLength(1)
+})
+
+test('Invalid route should redirect to page not found', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={['/some-invalid-routes']}>
+      <PageRouter />
+    </MemoryRouter>
+  )
+  expect(wrapper.find(Home)).toHaveLength(0)
+  expect(wrapper.find(PageNotFound)).toHaveLength(1)
+});
+
+test('Valid route for the homes list index page', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={['/homes']}>
+      <Homes />
+    </MemoryRouter>
+  )
+  expect(wrapper.find(PageNotFound)).toHaveLength(0)
+  expect(wrapper.find(Homes)).toHaveLength(1)
+})
