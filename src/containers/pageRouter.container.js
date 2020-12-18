7e5a71e1516982e9217729ea115444d04cb8332c
lekegitrepo <lekedevrepo@gmail.com>
2020-12-18 13:55:31 -0600

    create registration and login routes

--- src/containers/pageRouter.container.js
+++ src/containers/pageRouter.container.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../store/index.store';
-import Home from '../components/pages/Home'
-import PageNotFound from '../components/pages/PageNotFound.page'
-import Homes from '../components/pages/Homes.index.page'
-import HomeDetail from '../components/pages/Home.show.page'
-import FavouriteHomes from '../components/pages/FavouriteHomes'
+import Home from '../components/pages/Home';
+import PageNotFound from '../components/pages/PageNotFound.page';
+import Homes from '../components/pages/Homes.index.page';
+import HomeDetail from '../components/pages/Home.show.page';
+import FavouriteHomes from '../components/pages/FavouriteHomes';
+import Login from '../components/auth/Login';
+import Registration from '../components/auth/Registration';
 
 const PageRouter = () => (
   <Provider store={store}>
@@ -14,6 +16,12 @@ const PageRouter = () => (
       <Switch>
         <Route exact path='/'>
           <Home />
+        </Route>
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+        <Route exact path='/registration'>
+          <Registration />
         </Route>
         <Route exact path='/homes'>
           <Homes />
