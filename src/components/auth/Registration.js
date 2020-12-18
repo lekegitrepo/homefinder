9b068a062673fb819784eadfc290e61e6c0442b1
lekegitrepo <lekedevrepo@gmail.com>
2020-12-18 13:57:03 -0600

    create Registration component

---
+++ src/components/auth/Registration.js
@@ -0,0 +1,10 @@
+// API URL: https://homefinderapi.herokuapp.com/api/v1/...
+import React from 'react';
+
+const Registration = () => (
+    <section>
+      <h3>Sign Up</h3>
+    </section>
+)
+
+export default Registration;
