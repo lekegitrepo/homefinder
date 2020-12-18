74d565a9fc1a246fea1f637a2112bfb0630161cd
lekegitrepo <lekedevrepo@gmail.com>
2020-12-18 13:42:12 -0600

    create pageRouters container and add the page routes

---
+++ src/containers/pageRouter.container.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store/index.store';
+import Home from '../components/pages/Home'
+import PageNotFound from '../components/pages/PageNotFound.page'
+import Homes from '../components/pages/Homes.index.page'
+import HomeDetail from '../components/pages/Home.show.page'
+import FavouriteHomes from '../components/pages/FavouriteHomes'
+
+const PageRouter = () => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/homes'>
+          <Homes />
+        </Route>
+        <Route exact path='/home/:id'>
+          <HomeDetail />
+        </Route>
+        <Route exact path='/favourites'>
+          <FavouriteHomes />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </Router>
+  </Provider>
+)
+
+export default PageRouter;
