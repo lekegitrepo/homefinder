400470146b315235e3bf5f78c74ddcd4c8e6dd0f
lekegitrepo <lekedevrepo@gmail.com>
2020-12-19 11:07:55 -0600

    create Places component

---
+++ src/components/Places.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const Places = ({ cardImages }) => (
+  <section>
+    <div>
+      {/*cardImages loop here*/}
+      <img src={''} alt='home image' className='home__image image1' />
+      <img src={''} alt='home image' className='home__image image2' />
+      <img src={''} alt='home image' className='home__image image3' />
+    </div>
+    <div>
+      <h4>Only best places for your rest</h4>
+      <p></p>
+      {/*START FREE button component here*/}
+    </div>
+  </section>
+)
+
+export default Places;
