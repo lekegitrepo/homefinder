3010edd4f46f260819bafb52e4f781c48596dacd
lekegitrepo <lekedevrepo@gmail.com>
2020-12-19 11:34:02 -0600

    create FullBgImage component

---
+++ src/components/FullBgImage.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FullBgImage = () => (
+  <section>
+    <h6>Be everywhere at home</h6>
+    <div>
+      {/*START FREE Button component here*/}
+    </div>
+  </section>
+)
+
+export default FullBgImage;
