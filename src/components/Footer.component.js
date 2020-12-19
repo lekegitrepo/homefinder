620303b21ae6201a41c730128decbdeee63e4257
lekegitrepo <lekedevrepo@gmail.com>
2020-12-19 11:22:53 -0600

    create Footer component

---
+++ src/components/Footer.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = ({ cardImages }) => (
+  <footer>
+    <div>
+      {/*Logo component here*/}
+    </div>
+    <p>{/*Address detail here*/}</p>
+    <div>
+      {/*social media icon: facebook twitter instagram*/}
+    </div>
+    <p className="copyright">HomeFinder 2020. All right reserved</p>
+  </footer>
+)
+
+export default Footer;
