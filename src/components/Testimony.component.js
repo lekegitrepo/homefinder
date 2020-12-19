13d80d7eafe51ef8b2c85b79a2fd696ea60aa036
lekegitrepo <lekedevrepo@gmail.com>
2020-12-19 11:46:02 -0600

    create Testimony component

---
+++ src/components/Testimony.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Testimony = () => (
+  <section>
+    <h6>PEOPLE ARE ALREADY ENJOIN THE SERVICE</h6>
+    <h3>Look what these people have to say</h3>
+    <div>
+      {/*Testimony card image of users*/}
+    </div>
+  </section>
+)
+
+export default Testimony;
