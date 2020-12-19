e4f12df27f3f3d6ad311285d383d0a74ae62a8c6
lekegitrepo <lekedevrepo@gmail.com>
2020-12-19 04:55:08 -0600

    create Header component

---
+++ src/components/Header.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Header = ({ navbar }) => (
+  <header>
+    <nav>{navbar}</nav>
+    <section>
+      <h3>Find Housing anywhere</h3>
+      <p>
+        The best offers for you at any point of your journey. Wherever you are,
+        feel yourself like at your own home.
+      </p>
+      <div>
+        <Link path='/registration' className='btn__main'>
+          START FREE
+        </Link>
+      </div>
+    </section>
+  </header>
+)
+
+export default Header;
