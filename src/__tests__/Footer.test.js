8f0902a6cf190c2f1a9d01fbcecf71ad1209f53c
lekegitrepo <lekedevrepo@gmail.com>
2020-12-19 11:33:35 -0600

    create FullBgImage component test

--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Footer from '../components/Footer.component'
 
-test('render the places component', () => {
+test('render the footer component', () => {
   const wrapper = shallow(<Footer />);
   const text = wrapper.find('p.copyright').text()
   expect(text).toEqual('HomeFinder 2020. All right reserved');
---
+++ src/__tests__/FullBgImage.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import FullBgImage from '../components/FullBgImage.component'
+
+test('render the FullBgImage component', () => {
+  const wrapper = shallow(<FullBgImage />);
+  const text = wrapper.find('h6').text()
+  expect(text).toEqual('Be everywhere at home');
+})
