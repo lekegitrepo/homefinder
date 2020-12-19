8f0d4df0293a15bf193c9f5c7fb0e80a635f00ca
lekegitrepo <lekedevrepo@gmail.com>
2020-12-19 10:49:06 -0600

    create Stats component

---
+++ src/components/Stats.component.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Stats = () => (
+    <section>
+      <div>
+        <h6>SIMPLE AND FAST WAY TO RENT</h6>
+        <h3>Comfortable home from reliable people</h3>
+      </div>
+      <div>
+        The best offers for you at any point of your journey. Wherever you are,
+        feel yourself like at your own home.
+      </div>
+      <div>
+        <img src={''} alt='stats image' className='stats__image' />
+      </div>
+    </section>
+)
+
+export default Stats;
