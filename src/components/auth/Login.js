b5fd594d5b258a92d8c7fd8850e6504f9a90aa3c
lekegitrepo <lekedevrepo@gmail.com>
2020-12-19 14:36:32 -0600

    add form element to the Login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => (
-  <section>
-    <h3>Login</h3>
-  </section>
-);
+const Login = () => {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = event => {
+    setState(prevProps => ({
+      ...prevProps,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(state);
+  };
+
+  return (
+    <section className="registration-form">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={state.email}
+          onChange={handleChange}
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          required
+        />
+        <input type="password" name="password" placeholder="Password" value={state.password} onChange={handleChange} required />
+
+        <button type="submit"> Register </button>
+      </form>
+    </section>
+  );
+};
 
 export default Login;
