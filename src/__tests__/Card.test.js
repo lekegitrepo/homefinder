e6671a2b6c8aff9019a21cf49ef8463c3e747c2c
lekegitrepo <lekedevrepo@gmail.com>
2020-12-21 13:30:25 -0600

    fix the path error

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Card from '../../components/auth/Card';
+import Card from '../components/Card.component';
 
 test('render the Card component', () => {
   const wrapper = shallow(<Card />);
