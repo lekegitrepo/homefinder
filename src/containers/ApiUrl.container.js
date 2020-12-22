2fb7b4882eae8f32537d87f532a0fc3e78424b4d
lekegitrepo <lekedevrepo@gmail.com>
2020-12-22 08:54:15 -0600

    create Api url container

---
+++ src/containers/ApiUrl.container.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+// API Endpoints
+const API_BASE_URL = 'https://homefinderapi.herokuapp.com/api/v1/';
+const SIGN_UP = 'https://homefinderapi.herokuapp.com/api/v1/sign_up';
+const SIGN_IN = 'https://homefinderapi.herokuapp.com/api/v1/sign_in';
+const RENT_HOMES = 'https://homefinderapi.herokuapp.com/api/v1/homes';
