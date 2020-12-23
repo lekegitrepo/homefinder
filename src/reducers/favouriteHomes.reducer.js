94308a9d4c8d2ff15bbe8c435ad3e31e134ddc70
lekegitrepo <lekedevrepo@gmail.com>
2020-12-23 07:14:23 -0600

    create the favourite homes reducer

---
+++ src/reducers/favouriteHomes.reducer.js
@@ -0,0 +1,14 @@
+const favouriteHomes = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_HOME':
+      return [...state, action.payload]
+    case 'REMOVE_HOME':
+      return state.filter(item => {
+        return item.id != action.payload.id
+      })
+    default:
+      return state
+  }
+}
+
+export default favouriteHomes;
