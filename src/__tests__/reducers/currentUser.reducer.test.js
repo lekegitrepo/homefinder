ac41c95c7e1506874f2cbac8136a7ec0e657f677
lekegitrepo <lekedevrepo@gmail.com>
2020-12-23 07:25:30 -0600

    create test for the current user state reducer

---
+++ src/__tests__/reducers/currentUser.reducer.test.js
@@ -0,0 +1,24 @@
+import currentUser from '../../reducers/currentUser.reducer';
+import actions from '../../actions/index.actions';
+
+const { userActions } = actions;
+
+const user = {
+  id: 1,
+  fullname: 'John Doe',
+  username: 'JohnD',
+  email: 'john@gmail.com'
+}
+
+describe('create user state', () => {
+  test('default state', () => {
+    const defaultState = currentUser({}, {});
+    expect(defaultState).toEqual({})
+  })
+
+  test('create state for the current user', () => {
+    const { createUser } = userActions;
+    const userCurrentState = currentUser({}, createUser(user));
+    expect(userCurrentState.user).toEqual(user)
+  })
+})
