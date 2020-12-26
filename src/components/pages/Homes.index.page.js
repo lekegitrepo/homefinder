98eda64779f6117ecf655f2ea9eeccad8182ca06
lekegitrepo <lekedevrepo@gmail.com>
2020-12-26 14:23:00 -0600

    implement the get request call for the homes list

--- src/components/pages/Homes.index.page.js
+++ src/components/pages/Homes.index.page.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+// import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getHomesListRequest } from '../../services/apiRequests.services';
 
 const Homes = () => {
-  const currentUser = useSelector(state => state.currentUser);
-  console.log('This is current user initial state Homes index page:', currentUser);
+  // const currentUser = useSelector(state => state.currentUser);
+  // console.log('This is current user initial state Homes index page:', currentUser);
+
+  const [list, setList] = useState([]);
+
+  // console.log('This is Home list', list);
+
+  useEffect(() => {
+    getHomesListRequest('homes').then(response => {
+      if (response.statusText === 'OK') {
+        setList(response.data);
+      } else {
+        console.log('todo populate registration error');
+      }
+    }).catch(error => {
+      console.log('registrations error', error);
+    });
+  }, [list]);
 
   return (
     <section>
