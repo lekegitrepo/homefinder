72a88310e0180d83fb10d6f6877c89aafaeee6c7
lekegitrepo <lekedevrepo@gmail.com>
2020-12-26 13:55:01 -0600

    create basic get request test for the homes list

--- src/__tests__/services/apiRequest.services.test.js
+++ src/__tests__/services/apiRequest.services.test.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { postFormRequest } from '../../services/apiRequests.services';
+import { postFormRequest, getHomesListRequest } from '../../services/apiRequests.services';
 
 jest.mock('axios');
 
-describe('API Requests', () => {
+describe('API POST Requests', () => {
   const resp = {
     id: 1,
     fullname: 'John Doe',
@@ -28,4 +28,43 @@ describe('API Requests', () => {
   test('post bad request for user', () => postFormRequest('sign_', resp).catch(err => {
     expect(err).toEqual(new Error());
   }));
+});
+
+describe('API GET Requests', () => {
+  const data = [
+    {
+      id: 1,
+      home: 'Apartment',
+      description: 'This is a beautiful house',
+      location: 'Urban',
+    },
+    {
+      id: 2,
+      home: 'Mansion',
+      description: 'This is a beautiful house',
+      location: 'Urban',
+    },
+    {
+      id: 3,
+      home: 'cottage',
+      description: 'This is a beautiful house',
+      location: 'Country side',
+    },
+    {
+      id: 4,
+      home: 'Manor',
+      description: 'This is a beautiful house',
+      location: 'Countryside',
+    },
+  ]
+
+  axios.get.mockResolvedValue({ data });
+  test('axios request', () => {
+    axios.get = jest.fn(() => Promise.resolve({ data }));
+
+    axios.get.mockImplementationOnce(() => Promise.resolve({ data }));
+
+    getHomesListRequest('homes');
+    expect(axios.get).toHaveBeenCalledTimes(1);
+  });
 });
