2653f4232f85456c25c640830c425fdb951b6a70
lekegitrepo <lekedevrepo@gmail.com>
2020-12-28 05:22:58 -0600

    fix the store provider error in Card component test

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import Card from '../components/Card.component';
 
-test('render the Card component', () => {
-  const wrapper = shallow(<Card />);
-  expect(wrapper.exists()).toBe(true);
-});
+
+describe('Render Card component', () => {
+  const initialState = {};
+  const mockStore = configureStore();
+  let store;
+
+  test('render the Card component', () => {
+    store = mockStore(initialState);
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Card />
+      </Provider>
+    );
+    expect(wrapper.exists()).toBe(true);
+  });
+})
+
