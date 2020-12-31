a4e0af11a3fb19a4e97a1fda46363d281f2b6957
lekegitrepo <lekedevrepo@gmail.com>
2020-12-31 15:41:52 -0600

    create Button component

---
+++ src/components/button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Button = ({ text, link }) => (<Link to={link} className="btn link">{text}</Link>);
+
+Button.defaultProps = {
+  text: '',
+  link: '#',
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  link: PropTypes.string,
+};
