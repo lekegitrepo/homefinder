e8db312dbd2696924be673a1416b5449f04b0849
lekegitrepo <lekedevrepo@gmail.com>
2020-12-31 08:47:56 -0600

    fix the axios delete body data problem

--- src/services/apiRequests.services.js
+++ src/services/apiRequests.services.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // API Endpoints
 const API_BASE_URL = 'https://homefinderapi.herokuapp.com/api/v1';
+// const API_BASE_URL = 'http://localhost:3001/api/v1';
 
 export const postFormRequest = (reqUrl, userData) => axios.post(`${API_BASE_URL}/${reqUrl}`,
   userData,
@@ -20,12 +21,17 @@ export const postFavouriteHomeRequest = (reqUrl, id, auth = '') => axios.post(`$
   .catch(error => error);
 
 export const getFavouriteHomesRequest = (reqUrl, auth = '') => axios.get(`${API_BASE_URL}/${reqUrl}`,
-  { withCredentials: true }).then(resp => resp).catch(error => error);
-
-export const deleteFavouriteHomeRequest = (reqUrl, id, auth = '') => axios.delete(`${API_BASE_URL}/${reqUrl}/`,
-  id,
   {
     headers: { Authenticate: auth },
+    withCredentials: true,
+  })
+  .then(resp => resp)
+  .catch(error => error);
+
+export const deleteFavouriteHomeRequest = (reqUrl, id, auth = '') => axios.delete(`${API_BASE_URL}/${reqUrl}/`,
+  {
+    headers: { Authenticate: auth },
+    data: id,
     withCredentials: true,
   })
   .then(resp => resp)
