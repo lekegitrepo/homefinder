626b1ce374d1a3af68c045999d01589f05fae985
lekegitrepo <lekedevrepo@gmail.com>
2020-12-31 08:40:50 -0600

    update the remove action case

--- src/reducers/favouriteHomes.reducer.js
+++ src/reducers/favouriteHomes.reducer.js
@@ -1,9 +1,17 @@
 const favouriteHomes = (state = [], action) => {
   switch (action.type) {
     case 'ADD_HOME':
-      return [...state, action.payload];
+      return [...state,
+        {
+          fav: action.payload.obj,
+          homeId: action.payload.homeId,
+          picked: true,
+        },
+      ];
+    case 'FETCH_FAV_HOME':
+      return [...state];
     case 'REMOVE_HOME':
-      return state.filter(item => item.id !== action.payload.id);
+      return state.filter(item => item.homeId !== action.payload.id);
     default:
       return state;
   }
