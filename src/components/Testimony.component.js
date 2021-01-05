3894bc3f0330b8bbce725ba656c757e1c151d332
lekegitrepo <lekedevrepo@gmail.com>
2021-01-05 14:30:34 -0600

    add another UserCard component

--- src/components/Testimony.component.js
+++ src/components/Testimony.component.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import UserCard from './cards/UserCard.component';
+import user1 from '../images/user1.jpeg';
+import user2 from '../images/user2.jpeg';
+import user3 from '../images/user3.jpeg';
 
 const Testimony = () => (
   <section className="testimony">
@@ -9,17 +12,24 @@ const Testimony = () => (
     </div>
     <div className="testimony__cards">
       <UserCard
-        imagePath="#"
+        imagePath={user1}
         name="John Doe"
         ocupation="Programmer"
         testimony="Lorem ipsum est consequat qui ex sit nostrud adipisicing dolor irure duis in sunt ea proident eiusmod dolore deserunt qui in."
       />
 
       <UserCard
-        imagePath="#"
+        imagePath={user3}
         name="Jane Doe"
         ocupation="Designer"
         testimony="Officia occaecat occaecat sunt laborum est dolor amet deserunt dolore exercitation ex non occaecat eu consequat."
+      />
+
+      <UserCard
+        imagePath={user2}
+        name="Mark Spencer"
+        ocupation="Artist"
+        testimony="Ea sint consectetur magna commodo minim adipisicing ut dolore anim laboris nostrud ut."
       />
     </div>
   </section>
