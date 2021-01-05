0fcdae666a9225e4d4ecd9773ac2166085e709f7
lekegitrepo <lekedevrepo@gmail.com>
2021-01-05 09:45:21 -0600

    create Logo component

---
+++ src/components/Logo.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Logo = () => (
+  <div className="header__logo">
+    <div className="header__logo-text">HF</div>
+    <div className="header__logo-name">HomeFinder</div>
+  </div>
+);
+
+export default Logo;
