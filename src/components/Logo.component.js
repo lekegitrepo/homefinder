b85c140b77eb3d484bc490d988fd20b2fc9b4422
lekegitrepo <lekedevrepo@gmail.com>
2021-01-09 04:21:56 -0600

    update the logo component structure and style

--- src/components/Logo.component.js
+++ src/components/Logo.component.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = () => (
-  <div className="header__logo">
-    <div className="header__logo-text">HF</div>
-    <div className="header__logo-name">HomeFinder</div>
+  <div className="logo">
+    <Link to="/" className="logo-link">
+      <div className="logo-text">HF</div>
+      <div className="logo-name">HomeFinder</div>
+    </Link>
   </div>
 );
 
