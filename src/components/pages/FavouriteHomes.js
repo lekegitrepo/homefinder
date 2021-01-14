560a4493c123033c4a178fcc2a9caf4f16076671
lekegitrepo <lekedevrepo@gmail.com>
2021-01-14 04:02:33 -0600

    add condition to check the current user before favourite list can be display

--- src/components/pages/FavouriteHomes.js
+++ src/components/pages/FavouriteHomes.js
@@ -11,21 +11,32 @@ import actions from '../../actions/index.actions';
 const FavouriteHomes = () => {
   const dispatch = useDispatch();
   const currentUser = useSelector(state => state.currentUser);
-  const favouriteHomes = useSelector(state => state.favouriteHomes);
-  console.log('This is favouriteHomes state outside:', favouriteHomes);
+  // const favouriteHomes = useSelector(state => state.favouriteHomes);
   const [list, setList] = useState({ favourites: [] });
-  console.log('This is List outside:', list);
+
+  if (!currentUser.user) {
+    return (
+      <section className="">
+        <Navbar cssClass="u-padding-left u-bg-color" />
+        <div className="u-center-text">
+          <h3 className="heading-secondary">User not logged in</h3>
+          <p>
+            Please,
+            <Link to="/login"> log in </Link>
+            or
+            <Link to="/registration"> sign up </Link>
+            to view the list of your Favourite House(s)
+          </p>
+        </div>
+      </section>
+    );
+  }
 
   const fetchFavourites = async () => {
     await getFavouriteHomesRequest('favourites', currentUser.user.auth_token).then(res => {
       if (res.statusText === 'OK') {
         setList({ favourites: res.data.favourites });
         dispatch(actions.homeActions.selectAllFavourites(res.data.favourites));
-        // console.log('This is the res from favouriteHomes:', res.data.favourites);
-        // console.log('This is the List:', list);
-        // setLoading(true);
-        // console.log('This is favourite inside res:', favouriteHomes);
-        // console.log('This is List inside:', list);
       } else {
         console.log('todo populate registration error');
       }
@@ -47,13 +58,19 @@ const FavouriteHomes = () => {
       <div className="home-list__row">
         {
           (list.favourites.length === 0)
-            ? <h3>No Favourite Houses</h3>
+            ? (
+              <h3 className="instruction">
+                You have not picked any Favourite House.
+                <p>
+                  View the list of available
+                  <Link to="/homes" className="link"> Houses </Link>
+                  and pick your Favourite House(s)
+                </p>
+              </h3>
+            )
             : list.favourites.map(item => <Card key={v4()} detail={item} likeButton={false} />)
         }
       </div>
-      <Link to="/">
-        Back to Home page
-      </Link>
     </section>
   );
 };
--- src/reducers/favouriteHomes.reducer.js
+++ src/reducers/favouriteHomes.reducer.js
@@ -1,10 +1,8 @@
 const favouriteHomes = (state = { favourites: [] }, action) => {
-  // console.log('This is state object from reducer 1:', state);
   switch (action.type) {
     case 'ADD_HOME':
       return { ...state, favourites: [...state.favourites, action.payload] };
     case 'ALL_FAV_HOMES':
-      console.log('This is state object from reducer 2:', { ...state, favourites: [...action.payload] });
       return { ...state, favourites: [...action.payload] };
     case 'REMOVE_HOME':
       return state.favourites.filter(item => item.id !== action.payload.id);
