deb81aaa8155c8e316dd099466739a98dd2c2422
lekegitrepo <lekedevrepo@gmail.com>
2021-01-14 11:00:29 -0600

    update the error check in the FavouriteHome page

--- src/components/pages/FavouriteHomes.js
+++ src/components/pages/FavouriteHomes.js
@@ -1,57 +1,19 @@
 /* eslint-disable camelcase */
 // import React, { useState, useEffect } from 'react';
-import { useSelector/* , useDispatch */ } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { v4 } from 'uuid';
-// import { getFavouriteHomesRequest } from '../../services/apiRequests.services';
 import Card from '../cards/Card.component';
 import Navbar from '../Navbar.component';
 import Error from '../Error.component';
-// import actions from '../../actions/index.actions';
 
 const FavouriteHomes = () => {
-  // const dispatch = useDispatch();
   const currentUser = useSelector(state => state.currentUser);
   const list = useSelector(state => state.favouriteHomes);
-  // const [list, setList] = useState(favouriteHomes);
-  // console.log('This is favourite Homes current state:', favouriteHomes);
 
   if (!currentUser.user) {
-    return (
-      <Error content={
-        (
-          <>
-            <h3 className="heading-secondary">User not logged in</h3>
-            <p>
-              Please,
-              <Link to="/login"> log in </Link>
-              or
-              <Link to="/registration"> sign up </Link>
-              to view the list of your Favourite House(s)
-            </p>
-          </>
-        )
-      }
-      />
-    );
+    return (<Error content="to view the list of your favourites houses" title="" nav />);
   }
-
-  /* const fetchFavourites = async () => {
-    await getFavouriteHomesRequest('favourites', currentUser.user.auth_token).then(res => {
-      if (res.statusText === 'OK') {
-        setList({ favourites: res.data.favourites });
-        dispatch(actions.homeActions.selectAllFavourites(res.data.favourites));
-      } else {
-        console.log('todo populate registration error');
-      }
-    }).catch(error => {
-      console.log('registrations error', error);
-    });
-  };
-
-  useEffect(() => {
-    fetchFavourites();
-  }, [setList]); */
 
   return (
     <section className="home-list">
