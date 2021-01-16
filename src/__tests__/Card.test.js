d3f2cb975aaadc8c08a5b8111cfe236a3b27f5aa
lekegitrepo <lekedevrepo@gmail.com>
2021-01-16 14:00:41 -0600

    fix the Card test error

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import Card from '../components/Card.component';
+import Card from '../components/cards/Card.component';
 
 describe('Render Card component', () => {
   const initialState = {};
